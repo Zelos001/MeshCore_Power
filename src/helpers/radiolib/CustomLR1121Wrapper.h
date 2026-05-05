cf6248d65958bfdd462bc5ab450bb99e548f2723
Jakob (Jack/XDjackieXD) <jakob@chaosfield.at>
2026-05-05 18:10:47 +0200

    Add support for LilyGo T3 S3 LR1121

--- src/helpers/radiolib/CustomLR1121Wrapper.h
+++ src/helpers/radiolib/CustomLR1121Wrapper.h
@@ -7,6 +7,15 @@
 class CustomLR1121Wrapper : public RadioLibWrapper {
 public:
   CustomLR1121Wrapper(CustomLR1121& radio, mesh::MainBoard& board) : RadioLibWrapper(radio, board) { }
+
+  void setParams(float freq, float bw, uint8_t sf, uint8_t cr) override {
+    ((CustomLR1121 *)_radio)->setFrequency(freq);
+    ((CustomLR1121 *)_radio)->setSpreadingFactor(sf);
+    ((CustomLR1121 *)_radio)->setBandwidth(bw);
+    ((CustomLR1121 *)_radio)->setCodingRate(cr);
+    updatePreamble(sf);
+  }
+
   void doResetAGC() override { lr11x0ResetAGC((LR11x0 *)_radio, ((CustomLR1121 *)_radio)->getFreqMHz()); }
   bool isReceivingPacket() override {
     return ((CustomLR1121 *)_radio)->isReceiving();
