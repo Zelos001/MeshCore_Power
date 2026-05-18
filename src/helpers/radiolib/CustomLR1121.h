967591d63c9c755cd2fd8a7a5ba2dd45f8ce8648
radmir <39234121+mrrdmr@users.noreply.github.com>
2026-05-18 11:50:11 +0300

    Custom LR1121 radiolib helper
    from https://github.com/BetaFloof/MeshCore/tree/lilygo_t3s3_LR1121

---
+++ src/helpers/radiolib/CustomLR1121.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <RadioLib.h>
+#include "MeshCore.h"
+
+class CustomLR1121 : public LR1121 {
+  bool _rx_boosted = false;
+  public:
+    CustomLR1121(Module *mod) : LR1121(mod) { }
+
+    float getFreqMHz() const { return freqMHz; }
+
+    int16_t setRxBoostedGainMode(bool en) {
+      _rx_boosted = en;
+      return LR1121::setRxBoostedGainMode(en);
+    }
+
+    bool getRxBoostedGainMode() const { return _rx_boosted; }
+
+    bool isReceiving() {
+      uint16_t irq = getIrqStatus();
+      bool detected = ((irq & RADIOLIB_LR11X0_IRQ_SYNC_WORD_HEADER_VALID) || (irq & RADIOLIB_LR11X0_IRQ_PREAMBLE_DETECTED));
+      return detected;
+    }
+};
